7d74bd48e8db71126f5f10f9776ed2b9c8646ba4
miroroo <miroror173.173@gmail.com>
2026-02-23 20:00:52 +0300

    Добавила тестовую сцену и тестовый предмет, добавила методы MouseDown, MouseEnter, MouseExit

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -1,24 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 // Этот скрипт вешается на GameObject, с которым можно взаимодействовать
 public class InteractableObject : MonoBehaviour
 {
-    public string objectName; // имя объекта (для диалогов)
+    private bool isMouseOver = false;
+    public string objectName;
 
+    void Update()
+    {
+        // Рейкаст из позиции мыши
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+
+        // Проверяем, попал ли луч в этот объект
+        if (hit.collider != null && hit.collider.gameObject == gameObject)
+        {
+            if (!isMouseOver)
+            {
+                isMouseOver = true;
+                OnMouseEnter(); // вызываем наш метод
+            }
+        }
+        else
+        {
+            if (isMouseOver)
+            {
+                isMouseOver = false;
+                OnMouseExit(); // вызываем наш метод
+            }
+        }
+
+        // Далее обработка клика (как уже есть)...
+        if (Mouse.current.leftButton.wasPressedThisFrame && hit.collider != null && hit.collider.gameObject == gameObject)
+        {
+            Interact();
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        Debug.Log("Мышь наведена на " + objectName);
+        // Здесь можно, например, изменить цвет спрайта:
+        // GetComponent<SpriteRenderer>().color = Color.yellow;
+    }
+
+    private void OnMouseExit()
+    {
+        Debug.Log("Мышь покинула " + objectName);
+        // Вернуть исходный цвет:
+        // GetComponent<SpriteRenderer>().color = Color.white;
+    }
     public virtual void Interact()
     {
         Debug.Log("Взаимодействие с " + objectName);
     }
 
-    private void OnMouseEnter()
-    {
-        // показываем текст
-    }
-
-    private void OnMouseExit()
-    {
-        // Возвращаем как было
-    }
 }
